5c32217453655e4109d672b44303c6056d04611d
Aashika <khatiwadaaashika12@gmail.com>
2025-05-05 14:46:11 +0545

    var

---
+++ variable.py
@@ -0,0 +1,12 @@
+x=5
+x="ams"
+X=8
+print("x=",x)
+print("X=",X) #This wont overwrite small x as python is case-sensiive
+"""This
+is
+3riple quote 
+which can be used as multiline"""
+# or use this everywhere in
+# eachline
+
