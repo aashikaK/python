397dcf4778de2523df5ad6d532b3dabb2c646517
Aashika <khatiwadaaashika12@gmail.com>
2025-06-25 14:17:24 +0545

    project

---
+++ friendshipchecker.py
@@ -0,0 +1,16 @@
+print("Friendship compatibilty checker")
+your_name= input('Enter your name: ')
+friend_name=  input("Enter your friend's name: ")
+combined_names= your_name+ friend_name
+length= len(combined_names)
+# logic
+if length%2==0:
+    print("Your friendship is as sweet as honey. Perfect")
+
+else :
+    print("Your friendship is unbreakable and wild! Hot duos")
+
+if (your_name==friend_name):
+    print(" Also Damn, you got the same name. Are you guys soulmates?")
+
+print("\n Thank you for using our friendship compatibility test...")
