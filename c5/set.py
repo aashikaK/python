b52c022cda0c83c35f66081a79a679c6746249c9
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 20:16:02 +0545

    chatpter5

--- c5/set.py
+++ c5/set.py
@@ -1,2 +1,7 @@
-s={1,2,3}
+s={1,2,3,"aas",1} #no duplicates allowed in set
 e=set() #creates empty set e={} creates empty dictionary
+print(len(s))
+s.remove(1)
+print(s)
+# s.pop() removes random elements since set is unordered..
+# s.clear() it makes set empty 
