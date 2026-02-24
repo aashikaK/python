bc78decdbff15a1e2cca57997e342cd2688abf77
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 16:48:25 +0545

    chapter 4

--- c4p/qtn2.py
+++ c4p/qtn2.py
@@ -1,6 +1,6 @@
 marks=[]
 for i in range(6):
-    mark=int(input(f'Enter mark of student {i+1}'))
+    mark=int(input(f'Enter mark of student {i+1}: '))
     marks.append(mark)
-
-print(marks.sort())
+marks.sort()
+print(marks)
