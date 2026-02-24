f907c33bdcb43d6bd2f28fe32d81c350b8f67ed8
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 10:40:44 +0545

    average

--- chapter2practice/average_05.py
+++ chapter2practice/average_05.py
@@ -1,4 +1,4 @@
 a=int(input("Enter one number: "))
 b=int(input("Enter second number: "))
-average=(a+b)/2
+average=(a+b)/2 #If you want an integer result instead: Use // (floor division):
 print("The average of two numbers input by the user is: ",average)
