52672b81f8a823a491ebfe21d2146d08ef40253c
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 19:56:53 +0545

    chatpter5

---
+++ c5/dict.py
@@ -0,0 +1,12 @@
+marks={"Sita":100,
+       "Mina":22,
+       "Aashish":78
+}
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Sita":99,"Alina":56})
+print(marks)
+
+print(marks.get("Harry")) #returns none
+print(marks["Harry"]) #returns error
