d016162e3d4ee298b50d92b74188db1329f1c714
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 10:18:05 +0545

    inputfunction

---
+++ inputfun.py
@@ -0,0 +1,7 @@
+a= input('Enter a number: ')
+b= input('Enter another number: ')
+print(a+b)  #since a and b take string we get string as output so we typecast to get number
+
+c=int(input('Enter one number: '))
+d=int(input('Enter another number: '))
+print(c+d) #now it prints ans in integer form
