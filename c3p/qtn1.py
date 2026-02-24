116e6cf7bf6d8481c8de58585a0d3706b63d68e0
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 13:21:28 +0545

    chapter3

---
+++ c3p/qtn1.py
@@ -0,0 +1,3 @@
+name=input("Enter the name you want to display: ")
+name+=" good afternoon."
+print(name)
--- chapter2practice/square_06.py
+++ chapter2practice/square_06.py
@@ -1,3 +1,4 @@
 a=int(input("Enter a number to caculate its square: "))
-square=a*a
+# square=a*a 
+square=a**2 #its like a^2
 print("Square of ",a, " is: ",square)
