4d01b59f6acdf6fe0cf18935b000176800b67f3e
Aashika <khatiwadaaashika12@gmail.com>
2026-02-24 19:33:07 +0545

    chatpter4

--- c4p/qtn3.py
+++ c4p/qtn3.py
@@ -1,3 +1,3 @@
 tuple1=(1,2,3,4,"aashika")
-tuple1[4]="Lashika" #error cause tuple is immutable
+tuple1[4]="Lashika" #error cause tuple is immutable  -> TypeError: 'tuple' object does not support item assignment
 print(tuple1)
