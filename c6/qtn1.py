ebfbefdb6db9c18e87026fc28c6ed063b71a00c7
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 11:06:21 +0545

    conditional

--- c6/qtn1.py
+++ c6/qtn1.py
@@ -6,21 +6,21 @@ n4=int(input("Enter a number: "))
 if(n1>n2):
     if(n1>n3):
         if(n1>n4):
-            print(n1," is the greatest number")
+            print(n1,"is the greatest number.")
 
         else:
-            print(n4," is the greatest number.")
+            print(n4,"is the greatest number.")
     elif(n3>n4):
-        print(n3," is the greatest number.")
+        print(n3,"is the greatest number.")
     else:
-        print(n4," is the greatest number.")
+        print(n4,"is the greatest number.")
 elif(n2>n3):
     if(n2>n4):
         print(n2,"is the greatest number.")
     else:
-        print(n4, " is the greatest number.")
+        print(n4,"is the greatest number.")
 else:
     if(n3>n4):
-        print(n3, " is greatest number.")
+        print(n3,"is greatest number.")
     else:
-        print(n4, " is the greatest number.")
+        print(n4,"is the greatest number.")
