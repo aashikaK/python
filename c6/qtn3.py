06f6a8f1487925610d24df0d9ab99609fc4ec8cb
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 11:35:29 +0545

    conditional

--- c6/qtn3.py
+++ c6/qtn3.py
@@ -1,6 +1,14 @@
 spam=input("Enter the comment you got to check if it is spam: ")
 spam=spam.lower()
-if(spam=="make a lot of money" or spam=="buy now" or spam=="suscribe this" or spam=="click this"):
+# if(spam=="make a lot of money" or spam=="buy now" or spam=="suscribe this" or spam=="click this"):
+#     print("WARNING: It is spam.")
+# else:
+#     print("It is not spam.")
+
+
+# OR
+
+if("make a lot of money" in spam or "buy now" in spam  or "suscribe this" in spam  or "click this" in spam ):
     print("WARNING: It is spam.")
 else:
     print("It is not spam.")
