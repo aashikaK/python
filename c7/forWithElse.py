3341e66aaaef2113239d7261efcdaf36a3ec7882
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 15:08:57 +0545

    loop

--- c7/forWithElse.py
+++ c7/forWithElse.py
@@ -2,4 +2,4 @@ l=[1,2,3,4,5]
 for i in l:
     print(i)
 else:
-    print("done")  #it eectes affter for look exhausts
+    print("done")  #it eectes after for look exhausts only if it diesnot meet break
