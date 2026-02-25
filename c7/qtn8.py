7d45e42dba56f15f2e1113a7b78264a715873aea
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 20:14:30 +0545

    loop

--- c7/qtn8.py
+++ c7/qtn8.py
@@ -5,8 +5,7 @@
 '''
 n= int(input("Enter the value of n: "))
 for i in range(1,n+1):
-    if(i==1 or i==n):
-        star="*"*i
-        print(star, end="")
-        print("")
+    star="*"*i
+    print(star, end="")
+    print("")
     
