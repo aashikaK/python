ce6c3e0ff4213c9f66236559539f946df38df67e
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 14:20:39 +0545

    conditional

--- c6/qtn7.py
+++ c6/qtn7.py
@@ -1,3 +1,5 @@
 post="Aashika is the richest girl of her entire boodline. She earns millionns just in a day. Aashika has achieved ll her dreams. Aashika has many cars and houses. Aashika is happy now..."
-if("Aashika" in post):
+if("Aashika".lower() in post.lower()):
     print('This post is talking about Aashika.')
+else:
+    print("This post is not talking about Aashika.")
