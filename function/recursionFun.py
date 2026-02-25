37869059750c7ebc36bb1a100244af5d3d653af5
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 20:58:07 +0545

    function

---
+++ function/recursionFun.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    else:
+        return n*factorial(n-1)
+
+n=int(input("Enter a number to find its factorial: "))
+print(f'The factorial of {n} is: {factorial(n)}')
