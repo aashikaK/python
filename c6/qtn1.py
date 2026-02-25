1be73b4020793705b1334533d98ecf2c6e336034
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 11:10:07 +0545

    conditional

--- c6/qtn1.py
+++ c6/qtn1.py
@@ -3,24 +3,36 @@ n2=int(input("Enter a number: "))
 n3=int(input("Enter a number: "))
 n4=int(input("Enter a number: "))
 
-if(n1>n2):
-    if(n1>n3):
-        if(n1>n4):
-            print(n1,"is the greatest number.")
+# if(n1>n2):
+#     if(n1>n3):
+#         if(n1>n4):
+#             print(n1,"is the greatest number.")
 
-        else:
-            print(n4,"is the greatest number.")
-    elif(n3>n4):
-        print(n3,"is the greatest number.")
-    else:
-        print(n4,"is the greatest number.")
-elif(n2>n3):
-    if(n2>n4):
-        print(n2,"is the greatest number.")
-    else:
-        print(n4,"is the greatest number.")
-else:
-    if(n3>n4):
-        print(n3,"is greatest number.")
-    else:
-        print(n4,"is the greatest number.")
+#         else:
+#             print(n4,"is the greatest number.")
+#     elif(n3>n4):
+#         print(n3,"is the greatest number.")
+#     else:
+#         print(n4,"is the greatest number.")
+# elif(n2>n3):
+#     if(n2>n4):
+#         print(n2,"is the greatest number.")
+#     else:
+#         print(n4,"is the greatest number.")
+# else:
+#     if(n3>n4):
+#         print(n3,"is greatest number.")
+#     else:
+#         print(n4,"is the greatest number.")
+
+
+greatest = n1
+
+if n2 > greatest:
+    greatest = n2
+if n3 > greatest:
+    greatest = n3
+if n4 > greatest:
+    greatest = n4
+
+print(greatest, "is the greatest number.")
