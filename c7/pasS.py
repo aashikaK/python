5d334d62fcca81764de806c109b3585d2b0926a0
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 14:41:05 +0545

    loop

--- c7/pasS.py
+++ c7/pasS.py
@@ -1,5 +1,7 @@
 for i in range(10):
     pass # this means we will work on this loop afterwrds for nw pass this loop when needed we will work later on..
-
+else:
+    print("passed")
+    
 for i in range(5):
     print(i)
