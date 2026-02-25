2c85b9c103389aab19c6bb292663f97fff0420cb
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 11:21:14 +0545

    conditional

---
+++ c6/qtn2.py
@@ -0,0 +1,15 @@
+marks1=int(input("Enter marks of Science: "))
+marks2=int(input("Enter marks of Computer: "))
+marks3=int(input("Enter marks of Maths: "))
+
+# check for total percentages asuming total marks is 100 each
+tPercent=((marks1+marks2+marks3)/300)*100
+
+# m1Per=(marks1/100)*100
+# m2Per=(marks2/100)*100
+# m3Per=(marks3/100)*100
+
+if(tPercent>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("You are pass.")
+else:
+    print("You have failed.")
