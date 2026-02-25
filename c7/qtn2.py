8952ad56c5a577055ce5b57d916a2f6c6337e35b
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 14:56:49 +0545

    loop

--- c7/qtn2.py
+++ c7/qtn2.py
@@ -3,5 +3,4 @@ l=["Aashika","Anima","Ana","Maya"]
 for name in l:
     if(name.startswith("A")):
         print(f"Hello {name}")
-    else:
-        print("No name starts with A in this list.")
+    
