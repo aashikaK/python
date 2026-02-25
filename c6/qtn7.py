88fcc647329d6939ee0c5cb1c47a5edfc3c73ef6
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 14:17:23 +0545

    conditional

---
+++ c6/qtn7.py
@@ -0,0 +1,3 @@
+post="Aashika is the richest girl of her entire boodline. She earns millionns just in a day. Aashika has achieved ll her dreams. Aashika has many cars and houses. Aashika is happy now..."
+if("Aashika" in post):
+    print('This post is talking about Aashika.')
