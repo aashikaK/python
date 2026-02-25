570f63726a9c98d4327c17714f44db63f6996442
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 10:29:41 +0545

    dict/set

--- c5/qtn6.py
+++ c5/qtn6.py
@@ -4,3 +4,7 @@ for i in range(4):
     language=input("Enter your favourite language: ")
     languages.update({name:language})
 print(languages)  #or languages.item()
+
+
+# if keys are same fst one will be updated by the last one but if values are same it wont matter bcz keys are 
+# unique identifiers no he values
