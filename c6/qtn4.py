86b626cc3d24ee95ea56e2c13920264a21b171ca
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 11:39:29 +0545

    conditional

--- c6/qtn4.py
+++ c6/qtn4.py
@@ -2,3 +2,5 @@ username="aashika_k"
 unLength=len(username)
 if(unLength>10):
     print("The given username contains less than 10 characters.")
+else:
+    print("Characters are more or equal to 10")
