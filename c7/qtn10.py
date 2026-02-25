1f1405c81b6fbb1e2d44093aeed514efc0e4871c
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 20:25:38 +0545

    loop

--- c7/qtn10.py
+++ c7/qtn10.py
@@ -1,4 +1,4 @@
-n=int(input('Enter the number whose multiplication table youu want to print in reverse order: '))
+n=int(input('Enter the number whose multiplication table you want to print in reverse order: '))
 
-for i in range(n,0):
-    print(f'{n}*{i}={n*i}')
+for i in range(1,11):
+    print(f'{n} X {11-i} = {n*(11-i)}')
