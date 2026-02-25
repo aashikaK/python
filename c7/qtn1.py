e39e4ed818cf60ea395e39dbe9ba992b48957202
Aashika <khatiwadaaashika12@gmail.com>
2026-02-25 14:52:24 +0545

    loop

--- c7/qtn1.py
+++ c7/qtn1.py
@@ -1,5 +1,7 @@
 n=int(input("Enter a number whose multiplication table you want to generate: "))
 i=1
+
+# for i in range(1,11):
 while(i<=10):
     print(n," * ",i," = ",(n*i))
     i+=1
---
+++ chapter2practice/qtn2.py
@@ -0,0 +1,7 @@
+l=["Aashika","Anima","Ana","Maya"]
+
+for name in l:
+    if(name.startswith("A")):
+        print(f"Hello {name}")
+    else:
+        print("No name starts with A in this list.")
