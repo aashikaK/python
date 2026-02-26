1144ba6f5b2b0ccac6e1a8f01400f11aaff2c1d0
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 09:55:38 +0545

    function

--- function/qtn2.py
+++ function/qtn2.py
@@ -1,7 +1,7 @@
 
 def convert(celc):
     f=(celc*9/5)+32
-    print(f"The value of {celc} degree celcius in farhenheit is {round(f,2)}")
+    print(f"The value of {celc} degree celcius in Fahrenheit is {round(f,2)}")
 
 c=int(input("Enter the temperature in degree celcius: "))
 convert(c)
