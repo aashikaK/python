5681fac77681f63947ca1f8b66955cfd5905ac7d
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 20:57:50 +0545

    file

--- c9/filewrite.py
+++ c9/filewrite.py
@@ -3,3 +3,5 @@ st="Hi, this is Aashika Khatiwada aka AK the don. Haha, jokes apart, tell me how
 file=open("c9/myFile.txt", "w")
 file.write(st)
 print('Successfully carried out write operation..')
+
+# if we write again in the file that already has text it "w" will replace previous content with the new content
