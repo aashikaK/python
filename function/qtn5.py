6ef20f4fc10e1aeb19ed0e9a873306c6c2fb75b8
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 10:24:32 +0545

    function

--- function/qtn5.py
+++ function/qtn5.py
@@ -5,8 +5,9 @@
 def pattern():
     n=int(input("Enter the value of n: "))
     for i in range(1,n+1):
-        for j in range(i,n+1):
-            print('*', end="")
-        print("")
+        # for j in range(i,n+1):
+        #     print('*', end="")
+        # print("")
+        print("*"*(n-i+1))
 
 pattern()
