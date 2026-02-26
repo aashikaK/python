660266774f2c2760752433cdba36a2287525a14d
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 20:55:42 +0545

    file

--- c9/filewrite.py
+++ c9/filewrite.py
@@ -1,1 +1,5 @@
 st="Hi, this is Aashika Khatiwada aka AK the don. Haha, jokes apart, tell me how was your day?"
+
+file=open("c9/myFile.txt", "w")
+file.write(st)
+print('Successfully carried out write operation..')
