563f9e45eb842deef51d41dd34c83af4579d8386
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 21:03:25 +0545

    file

--- c9/fileread.py
+++ c9/fileread.py
@@ -1,6 +1,9 @@
 
 f=open("c9/file.txt")
-data=f.read()
-print(data)
+# data=f.read()
+# print(data)
+
+lines= f.readlines()
+print(lines,type(lines)) #readlines reads all line and returns a list (each line represent an element of list)
 f.close()
 
