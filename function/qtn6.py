45c37c5a46e8273a67d65c2b065081455ac186b9
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 11:32:50 +0545

    function

--- function/qtn6.py
+++ function/qtn6.py
@@ -4,4 +4,4 @@ def i_to_c(i):
 
 i=int(input("Enter the value of inch to convert into cm: "))
 cm=i_to_c(i)
-print(f'inch {i} converted to cm gives {cm}cm')
+print(f'{i}inch  converted to cm gives {cm}cm')
