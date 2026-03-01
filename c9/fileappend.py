b065762e6f1885f11b5efdecfb21e06e50dd1033
Aashika <khatiwadaaashika12@gmail.com>
2026-03-01 09:54:10 +0545

    file

--- c9/fileappend.py
+++ c9/fileappend.py
@@ -3,7 +3,8 @@ str="I hope it was good."
 file=open("c9/myFile.txt", "a")
 file.write(str)
 print('Successfully carried out append operation..')
-file.close()
+
 
 data=file.read()
 print(data)
+file.close()
