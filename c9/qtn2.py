4d5af5ccba4d5ae0536850851929dfb7e5fbe5d0
Aashika <khatiwadaaashika12@gmail.com>
2026-03-01 14:34:00 +0545

    file

--- c9/qtn2.py
+++ c9/qtn2.py
@@ -2,6 +2,7 @@ import random
 
 def game():
     print('Your are playing a game....')
+    score=random.randint(1,62)
     # fetch the highscore
     with open("c9/hiscore.txt") as f:
         hiscore=f.read()
@@ -9,8 +10,10 @@ def game():
             hiscore=int(hiscore)
         else:
             hiscore=0
-    score=random.randint(1,62)
     print(f'Your score: {score}')
+    if(score>hiscore):
+        with open("c9/hiscore.txt","w") as f:
+            f.write(str(score))
     return score
 
 game()
