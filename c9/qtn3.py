4ed6f18aa6ee1e0026c3b6745f1350ac71d84809
Aashika <khatiwadaaashika12@gmail.com>
2026-03-01 14:49:03 +0545

    file

--- c9/qtn3.py
+++ c9/qtn3.py
@@ -2,9 +2,9 @@ def generate(n):
     table=""
     for i in range(1,11):
         table=f"{n} X {i} = {n*i}"
-        with open(f"tables/table_{n}.txt","w") as f:
-            f.write(table)
+    with open(f"tables/table_{n}.txt","w") as f:
+        f.write(table)
     print('Tables generated successfully')
 
-for i in range(2,21):
-    generate(i)
+for n in range(2,21):
+    generate(n)
