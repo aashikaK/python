1e694f408a50fd3bb9d4a6801c23ff0d796a4fe7
Aashika <khatiwadaaashika12@gmail.com>
2026-03-01 10:01:25 +0545

    file

---
+++ c9/withSt.py
@@ -0,0 +1,11 @@
+f=open("c9/file.txt")
+print(f.read())
+f.close()
+
+# we can do same using with statement
+with open('c9/file.txt') as f:
+    f.readlines()
+
+    # Now using with  dont have to explicitely close the file
+
+ 
