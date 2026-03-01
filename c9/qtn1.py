b9a4c0d6a67d70e324fc9f10c9b7e06f17366fd0
Aashika <khatiwadaaashika12@gmail.com>
2026-03-01 10:12:18 +0545

    file

--- c9/qtn1.py
+++ c9/qtn1.py
@@ -1,6 +1,7 @@
 f=open('c9/poem.txt')
 str=f.read()
 if("twinkle" in str):
-    print("There is 'twinkle' in the poem.txt file")
-
+    print("There is 'twinkle' word present in the poem.txt file")
+else:
+     print("There is no 'twinkle' word present in the poem.txt file")
 f.close()
