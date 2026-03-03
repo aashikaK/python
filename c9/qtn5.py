be3fd1a7a2e9c3def09086d6426efb70b72e93e2
Aashika <khatiwadaaashika12@gmail.com>
2026-03-03 07:57:24 +0545

    files

--- c9/qtn5.py
+++ c9/qtn5.py
@@ -7,8 +7,12 @@ with open("c9/cencored.txt","r") as f:
 
 for word in words:
     text= text.replace(word,"#"*len(word))
-    
-    
+    text = text.replace(word.lower(), "#" * len(word))
+    text = text.replace(word.upper(), "#" * len(word))
+    # for word in words:
+    # pattern = re.compile(word, re.IGNORECASE)
+    # text = pattern.sub("#" * len(word), text)
+
 with open("c9/donkey.txt","w") as f:
     f.write(text)
 print("Successfully replaced 'donkey' with '#####'")
