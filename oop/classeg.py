66b2412ffa9f8fc214ac017959550f6442e51b6b
Aashika <khatiwadaaashika12@gmail.com>
2026-03-30 18:04:49 +0545

    oop

--- oop/classeg.py
+++ oop/classeg.py
@@ -9,3 +9,5 @@ print(e1.name,e1.salary)
 e2=Employee()
 e2.name="Binda" #this is an instance attribute
 print(e2.name,e2.age)
+
+# instance attributes take preferences over class attirbutes during assignmet or retrival
