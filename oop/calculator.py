1634bc73f40579eda67681aeef3c54a46ee899aa
Aashika <khatiwadaaashika12@gmail.com>
2026-03-30 19:38:55 +0545

    programmer's info

---
+++ oop/calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+    
+    def __init__(self,num):
+        self.num=num
+
+    def square(self):
+        print("Square= ",self.num*self.num)
+    def cube(self):
+        n=self.num
+        print("Cube= ",n*n*n)
+    def sqRt(self):
+        n=self.num
+        print("Square Root= ",n**1/2) #n^1/2
+
+c= Calculator(64)
+c.square()
+c.cube()
+c.sqRt()
