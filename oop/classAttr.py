6e0a937391318d14928b6eba5bb052462a92ac69
Aashika <khatiwadaaashika12@gmail.com>
2026-03-30 19:49:22 +0545

    class attribute

--- oop/classAttr.py
+++ oop/classAttr.py
@@ -1,6 +1,9 @@
 class Attribute:
     a=78 #class attribute
 
-at=Attribute()
-at.a=789 #object attribute
-print(at.a)
+o=Attribute()
+o.a=0 #object attribute
+print(o.a)
+
+# o.a=0-> it doesnot change class attribute 
+print(Attribute.a)
