8503ffc25f6dd022e9e177a607f54b4d7e41f57e
Aashika <khatiwadaaashika12@gmail.com>
2026-03-30 18:19:23 +0545

    oop self

---
+++ oop/self.py
@@ -0,0 +1,13 @@
+class Student:
+    def set_name(self, name):
+        self.name = name
+
+    def show(self):
+        print("My name is", self.name)
+
+s1 = Student()
+s2 = Student()
+s1.set_name("Aashika")
+# Student.set_name(s1, "Aashika") python does this internally
+s2.set_name("Ram")
+#Student.set_name(s2, "Ram") python does this internally
