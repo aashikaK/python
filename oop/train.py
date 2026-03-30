3a0461c611f6ae8720f3d27418174132b262945e
Aashika <khatiwadaaashika12@gmail.com>
2026-03-30 20:07:58 +0545

    train info

--- oop/train.py
+++ oop/train.py
@@ -3,13 +3,15 @@ from random import randint
 class Train:
 
     def book(self,trainNo,source,to):
-        print(f"Ticket is booked in train number: {self.trainNo}, from {self.source} to {self.to}.")
+        print(f"Ticket is booked in train number: {trainNo}, from {source} to {to}.")
 
     def getStatus(self, trainNo):
+        self.trainNo=trainNo
         print(f"Train no: {self.trainNo} is running successfully  on time.")
 
     def getFare(self, trainNo, source, to):
-        print(f"Ticket fare in train no: {self.trainNo} from {self.source} to {self.to} is {randInt(222,5555)}")
+        self.source=source
+        print(f"Ticket fare in train no: {trainNo} from {self.source} to {to} is {randint(222,5555)}")
 
 t= Train()
 t.book(2,'Kathmandu','Chitwan')
