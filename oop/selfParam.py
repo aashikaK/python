8b510759acba6f5d20458ea45ae12def808d232a
Aashika <khatiwadaaashika12@gmail.com>
2026-03-30 20:16:35 +0545

    train info

--- oop/selfParam.py
+++ oop/selfParam.py
@@ -2,19 +2,19 @@ from random import randint
 
 class Train:
    class Train:
-    def __init__(self, tn):
-        self.tn = tn
+    def __init__(slf, tn):
+        slf.tn = tn
 
-    def book(self, source, to):
-        self.source = source
-        self.to = to
-        print(f"\nTicket is booked in train number: {self.tn}, from {source} to {to}.\n")
+    def book(slf, source, to):
+        slf.source = source
+        slf.to = to
+        print(f"\nTicket is booked in train number: {slf.tn}, from {source} to {to}.\n")
 
-    def getStatus(self):
-        print(f"\nTrain no: {self.tn} is running successfully on time.\n")
+    def getStatus(slf):
+        print(f"\nTrain no: {slf.tn} is running successfully on time.\n")
 
-    def getFare(self):
-        print(f"\nTicket fare in train no: {self.tn} from {self.source} to {self.to} is {randint(222,5555)}\n")
+    def getFare(slf):
+        print(f"\nTicket fare in train no: {slf.tn} from {slf.source} to {slf.to} is {randint(222,5555)}\n")
 
 t= Train(2)
 t.book('Kathmandu','Chitwan')
