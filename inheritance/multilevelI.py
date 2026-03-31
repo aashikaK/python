1fa5c4c87a6048219931f0ca56d994dc2f9d1973
Aashika <khatiwadaaashika12@gmail.com>
2026-03-31 20:16:35 +0545

    multilevel inheritance

---
+++ inheritance/multilevelI.py
@@ -0,0 +1,16 @@
+class Living:
+    @staticmethod
+    def does():
+        print("Living Beings breathe.")
+
+class Animal(Living):
+    def sound(self,name,sound):
+        print(f"{name} makes {self.sound} sound.")
+
+class Dog(Animal):
+    pass
+
+d=Dog()
+d.sound('dog','Barks')
+a=Animal()
+a.does()
