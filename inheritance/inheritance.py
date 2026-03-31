6f2a0108d40d154096c5d567c5e6a0f3089c0065
Aashika <khatiwadaaashika12@gmail.com>
2026-03-31 19:34:13 +0545

    inheritance

--- inheritance/inheritance.py
+++ inheritance/inheritance.py
@@ -1,7 +1,8 @@
 class Employee:
     emp_no=2
     def info(self,company):
-        print(f"Employee of company: {company}")
+        self.company=company
+        print(f"Employee of company: {self.company}")
 class Prog(Employee):
     def info(self):
         print(f"Employee:{self.emp_no} of company: {self.company}")
@@ -9,4 +10,5 @@ class Prog(Employee):
 e=Employee()
 e.info("Tech")
 p=Prog()
+p.company="Tech"
 p.info()
