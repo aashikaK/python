5343904759a9b7a2cb5968e4d7b407a82108b0f5
Aashika <khatiwadaaashika12@gmail.com>
2026-04-01 21:00:37 +0545

    q1

---
+++ inheritance/qtn1.py
@@ -0,0 +1,17 @@
+class TwoDVector:
+    def __init__(self,i,j):
+        self.i=i
+        self.j=j
+    def show(self):
+        print(f"The value of i and j are:{self.i} and {self.j}")
+
+class ThreeDVector(TwoDVector):
+    def __init__(self,i,j,k):
+        super.__init__(i,j)
+        self.k=k
+
+    def show(self):
+        print(f"The value of i,j and k are:{self.i},{self.j}and {self.k}")
+
+a=TwoDVector(1,2,3)
+b=ThreeDVector(3,4,5)    
