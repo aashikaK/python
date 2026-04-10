12acaa9b8a3bcb80909512c115acda94822cfc13
Aashika <khatiwadaaashika12@gmail.com>
2026-04-10 12:10:58 +0545

    inheritance4

--- inheritance/qtn4.py
+++ inheritance/qtn4.py
@@ -1,6 +1,6 @@
 class Employee:
     salary=60000
-    increment:20
+    increment=20
     @property
     def salaryAfterIncrement(self):
         return (self.salary+(self.salary*(self.increment/100)))
