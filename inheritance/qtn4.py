ba92bacfde5f56e7558c222c798ffd0062c273ef
Aashika <khatiwadaaashika12@gmail.com>
2026-04-10 13:04:14 +0545

    inheritance4

--- inheritance/qtn4.py
+++ inheritance/qtn4.py
@@ -6,9 +6,15 @@ class Complex:
     def __add__(self, other):
         return Complex(self.r+other.r, self.i+other.i)
     
+    def __mul__(self, other):
+        real = self.r * other.r - self.i * other.i
+        imag = self.r * other.i + self.i * other.r
+        return Complex(real, imag)
+    
     def __str__(self):
         return f"{self.r} +{self.i}i"
 
 c1=Complex(1,2)
 c2=Complex(4,5)
-print(c1+c2)
+print(f"Addition: {c1+c2}")
+print(f"Multiplication: {c1*c2}")
