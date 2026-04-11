9cfde1c979b08246c65c068614daecbc76c6bdbd
Aashika <khatiwadaaashika12@gmail.com>
2026-04-11 19:49:24 +0545

    type annotation

--- advanced/typeAnnotation.py
+++ advanced/typeAnnotation.py
@@ -1,1 +1,9 @@
 # Type annotation is a way to tell what type a variable should have using :
+# n: int = 7
+# n = "hello"   # ❗ still allowed
+
+name: str = "Aashika"
+marks: float = 85.5
+is_pass: bool = True
+
+print(name,marks,is_pass)
