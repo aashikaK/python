cac1a33f031be3373c8cb53b7fe0c3bb82342ef1
Aashika <khatiwadaaashika12@gmail.com>
2026-04-11 19:58:37 +0545

    typingModule

---
+++ advanced/typingModule.py
@@ -0,0 +1,9 @@
+# typing is a Python module that gives extra tools for type hints, especially for complex data types.
+from typing import List, Dict, Tuple, Optional, Union
+numbers: List[int] = [1, 2, 3]
+
+student: Dict[str, int] = {
+    "marks": 90
+}
+
+point: Tuple[int, int] = (10, 20)
