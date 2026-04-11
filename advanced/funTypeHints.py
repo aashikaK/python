66483b3f0ae0f71dc3661e0fa0d4c7b64ec42ea6
Aashika <khatiwadaaashika12@gmail.com>
2026-04-11 19:53:28 +0545

    typehintsInFunction

--- advanced/funTypeHints.py
+++ advanced/funTypeHints.py
@@ -5,3 +5,9 @@ def add(a:int,b:int)->int:
     return a+b
 
 print(add(5,6))
+
+# Type hints are NOT enforced in Python
+def add(a: int, b: int) -> int:
+    return a + b
+
+print(add("2", "3"))  # still runs! 
