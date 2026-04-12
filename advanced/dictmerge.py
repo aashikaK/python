bd23424690a8bb7b6eceecb3d903dd2ec5c5a2bc
Aashika <khatiwadaaashika12@gmail.com>
2026-04-12 10:25:33 +0545

    dictionary merge

--- advanced/dictmerge.py
+++ advanced/dictmerge.py
@@ -1,6 +1,13 @@
 # Merge Operator (|) Combines two dictionaries into a new dictionary
 
 d1={"a":1,"b":2}
-d2={"b":7,"c":3}
-result=d1|d2
+d2={"b":7,"c":3} 
+result=d1|d2 #right side dictionary d2's b overlaps d1's b and b becomes 7
 print(result)
+
+# Update Operator (|=) Updates the original dictionary
+d1 = {"a": 1, "b": 2}
+d2 = {"b": 3, "c": 4}
+
+d1 |= d2
+print(d1)
