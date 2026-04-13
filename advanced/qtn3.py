8d3a380b09da6733397bb3416bdac48f83c8a713
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 20:38:22 +0545

    qtn3

--- advanced/qtn3.py
+++ advanced/qtn3.py
@@ -4,8 +4,8 @@ except ValueError as v:
     print(v)
 
 else:
-    list=[1,2,3,4,5,6,7,8,9,10]
-    multiplication=[]
-    for i in list:
-        multiplication.append(n*i)
+    # list=[1,2,3,4,5,6,7,8,9,10]
+    # multiplication=[]
+    # for i in list:
+    multiplication=[i*n for i in range(1,11)]
     print(multiplication)
