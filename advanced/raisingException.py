a9ec57deb9690580fd724b1c5714a812097566ef
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 17:03:41 +0545

    raising exception

--- advanced/raisingException.py
+++ advanced/raisingException.py
@@ -2,3 +2,7 @@ age=int(input("Enter a number: "))
 
 if(age<0):
     raise ValueError("Age cannot be negative..")
+
+# raise TypeError("Wrong type")
+# raise ZeroDivisionError("Cannot divide")
+# raise Exception("General error")
