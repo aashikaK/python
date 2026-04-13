88077dfd1fd5fc91a80eef2dc1d2cec45defa189
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 20:36:33 +0545

    list comprehension

--- advanced/listComprehension.py
+++ advanced/listComprehension.py
@@ -6,6 +6,7 @@ l=[4,2,3,5,6,10]
 #     squaredList.append(i*i)
 # print(squaredList)
 # This can be done by list comprehension as:
+# List comprehension = a short way to create a list using a loop in ONE line.
 
 squaredList=[i*i for i in l]
 print(squaredList)
