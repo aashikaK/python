800311d5dbdf4a3a2205fdd4986916182ab4d86f
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 20:17:51 +0545

    creating new list from existing list

--- advanced/listComprehension.py
+++ advanced/listComprehension.py
@@ -1,7 +1,11 @@
 l=[4,2,3,5,6,10]
 
-squaredList=[]
+# squaredList=[]
 
-for i in l:
-    squaredList.append(i*i)
+# for i in l:
+#     squaredList.append(i*i)
+# print(squaredList)
+# This can be done by list comprehension as:
+
+squaredList=[i*i for i in l]
 print(squaredList)
