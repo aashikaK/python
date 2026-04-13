87e60a34533c04fcbb24e4f36f18e8d1ea086302
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 17:34:30 +0545

    __name__

--- advanced/nameMainModule/module.py
+++ advanced/nameMainModule/module.py
@@ -2,5 +2,5 @@ def hey():
     print("This is a function..")
 
 hey()
-print(__name__)  #this prints main if it file in which the function is runs and module when importing file runs..
+print(__name__)  #this prints __main__ if it file in which the function is runs and module when importing file runs..
                 # ... see importing.py by running it and this to see difference
