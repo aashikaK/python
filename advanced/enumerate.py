a252ec5d43563ac2c594238355d5696a9bab2760
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 20:13:15 +0545

    enumerate function

--- advanced/enumerate.py
+++ advanced/enumerate.py
@@ -1,6 +1,11 @@
 l=[4,5,6,7,8,98]
 
-index=1
-for i in l:
-    print(f"The item in index {index} is {i}")
-    index+=1
+# index=0
+# for i in l:
+#     print(f"The item in index {index} is {i}")
+#     index+=1
+
+# This can be simplified using enumerate function
+
+for index,item in enumerate(l):
+    print(f"The item in index {index} is {item}")
