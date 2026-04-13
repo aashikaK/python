810e4c6b5445464320f025701e88037bb54e69c0
Aashika <khatiwadaaashika12@gmail.com>
2026-04-13 17:22:55 +0545

    try with finally

--- advanced/tryFinally.py
+++ advanced/tryFinally.py
@@ -1,10 +1,28 @@
-# finally block ALWAYS executes no mater what:error, no error
+# # finally block ALWAYS executes no mater what:error, no error
+
+# try:
+#     n=int(input("Enter a number: "))
+
+# except ValueError as v:
+#     print(v)
+
+# finally:
+#     print("Hello!")
+
+# Why use finally?
+
+# 👉 For cleanup work, like:
+
+# closing files 📁
+# closing database connections
+# releasing resources 
 
 try:
-    n=int(input("Enter a number: "))
-
-except ValueError as v:
-    print(v)
-
+    file = open("test.txt")
+    data = file.read()
+except FileNotFoundError:
+    print("File not found")
 finally:
-    print("Hello!")
+    print("Closing file")
+    file.close()
+
