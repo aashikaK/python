e4cfa0714f47418ef2463bc6d546a359ada49177
Aashika <khatiwadaaashika12@gmail.com>
2026-04-15 18:12:48 +0545

    qtn4 chapter 13

--- advanced/c13qtn4.py
+++ advanced/c13qtn4.py
@@ -1,6 +1,6 @@
 # mul=[7,14,21,27,35,42,49,56,63,70]
 
-table=[str(7*i for i in range(1,11))]  #str bcz join fnction needs list of string
+table=[str(7*i) for i in range(1,11)]  #str bcz join fnction needs list of string
 
 s="\n".join(table)
 print(s)
