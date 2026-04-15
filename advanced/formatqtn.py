b24a286a67f55e2cf19b2e8e9e9c0784ca1bc18a
Aashika <khatiwadaaashika12@gmail.com>
2026-04-15 18:01:15 +0545

    qtn for format

---
+++ advanced/formatqtn.py
@@ -0,0 +1,6 @@
+name=input("Enter name of the student: ")
+marks=int(input('Enter marks of the student: '))
+num=int(input('Enter phone number of the student: '))
+
+str="\"The name of the student is {}, his mark is {} and phone number is {}".format(name,marks,num)
+print(str)
