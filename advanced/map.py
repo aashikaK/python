582d3fb63461b1d4743e1782fe8e9246a391989a
Aashika <khatiwadaaashika12@gmail.com>
2026-04-15 13:24:47 +0545

    map

--- advanced/map.py
+++ advanced/map.py
@@ -2,5 +2,18 @@
 
 num=[1,2,3,4,5,6,7]
 
+# def double(x):
+#     return x * 2
+
+# result = map(double, nums)  #(function,iterable)
+# print(list(result))
+
+# with lambda same::
 sum=map(lambda x:x+1,num)
 print(list(sum))
+
+# for strings
+names = ["ram", "shyam", "hari"]
+
+result = list(map(lambda x: x.upper(), names))
+print(result)
